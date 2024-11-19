76566f8c970fa26c6cad145a8416794ef837d157
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-19 12:08:14 +0800

    Create route handlers to render all ejs pages

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+	res.render("homepage.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+})
+
+app.get("/userpage", (req, res) => {
+    res.render("userpage.ejs");
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
