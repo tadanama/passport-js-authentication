7ee94e1092e7fd1c459ffdb84df4007133c19427
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-19 12:10:17 +0800

    Add comments to the code

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Render the homepage
 app.get("/", (req, res) => {
 	res.render("homepage.ejs");
 });
 
+// Render the login page
 app.get("/login", (req, res) => {
-    res.render("login.ejs");
-})
+	res.render("login.ejs");
+});
 
+// Render the sign up page
 app.get("/signup", (req, res) => {
-    res.render("signup.ejs");
-})
+	res.render("signup.ejs");
+});
 
+// Render the user page
 app.get("/userpage", (req, res) => {
-    res.render("userpage.ejs");
-})
+	res.render("userpage.ejs");
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
